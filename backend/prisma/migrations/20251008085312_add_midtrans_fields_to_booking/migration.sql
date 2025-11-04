9e678c1ed3c5270b388c7acb045c98d5e7a541e7
idamansyahs <idamansyahs.berprogram@gmail.com>
2025-11-04 07:54:09 +0700

    Menambahkan semua file proyek

--- backend/prisma/migrations/20251008085312_add_midtrans_fields_to_booking/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `booking` ADD COLUMN `paymentRedirectUrl` TEXT NULL,
-    ADD COLUMN `paymentToken` TEXT NULL;
--- backend/prisma/migrations/20251009205911_add_payment_status_to_booking/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `booking` ADD COLUMN `payment_status` VARCHAR(191) NOT NULL DEFAULT 'PENDING';
